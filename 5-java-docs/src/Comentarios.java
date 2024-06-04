f68a1ebc81cc28d16bfc052f4e10b1a4c0c166c9
OEscriba <fernadohtr@gmail.com>
2024-06-04 16:22:30 -0300

    feat: creates simples Java project to test the use of comments and documentation features of Java

---
+++ 5-java-docs/src/Comentarios.java
@@ -0,0 +1,19 @@
+public class Comentarios {
+    
+    public static void main(String[] args) {
+        //Este é um comentário de uma linha
+
+        /* Este é um 
+         * comentário 
+         * de várias linhas
+         */ 
+
+
+    }
+
+ /** Já este aqui é um comentário
+  * com finalidade de documentação.
+  * Ele usa dois asteriscos no começo
+  */
+
+}
